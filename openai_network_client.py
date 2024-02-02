640a1abfe7f827afafcbcc830921a0953907ff21
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-02-02 20:54:51 +0100

    `max_tokens` request property temporary disabled.

--- openai_network_client.py
+++ openai_network_client.py
@@ -53,7 +53,7 @@ class NetworkClient():
             'messages': internal_messages,
             'model': assitant_setting.chat_model,
             'temperature': assitant_setting.temperature,
-            'max_tokens': assitant_setting.max_tokens,
+            # 'max_tokens': assitant_setting.max_tokens,
             'top_p': assitant_setting.top_p,
             'stream': True
         })
