853a3e57c61e21f6b83c95fe52202f0348ea97b1
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-03-16 16:13:34 +0100

    Fixed bug where ST needs to be restarted to reflect in assistants quick pick panel changes made in a user settings. (e.g. now a user able to update its assistants settings live without a need of restarting sublime to apply them).

--- openai_panel.py
+++ openai_panel.py
@@ -4,7 +4,7 @@ from sublime import View, Region
 from sublime_plugin import WindowCommand
 from .errors.OpenAIException import WrongUserInputException, present_error
 import functools
-from typing import Optional
+from typing import Optional, List
 from .cacher import Cacher
 from .openai_worker import OpenAIWorker
 from threading import Event
@@ -16,10 +16,16 @@ class OpenaiPanelCommand(WindowCommand):
     def __init__(self, window):
         super().__init__(window)
         self.settings = sublime.load_settings("openAI.sublime-settings")
-        self.assistants = [
-            # Unpacking both dictionaries, combine them while overwriting default values with user setup and then initialize
-            # with a complete dict AssistantSettings struct.
-            AssistantSettings(**{**DEFAULT_ASSISTANT_SETTINGS, **assistant}) for assistant in self.settings.get('assistants')
+        # Load assistants from settings
+        self.load_assistants()
+
+        # Register a callback to reload assistants when settings change
+        self.settings.add_on_change("reload_assistants", self.load_assistants)
+
+    def load_assistants(self):
+        self.assistants: List[AssistantSettings] = [
+            AssistantSettings(**{**DEFAULT_ASSISTANT_SETTINGS, **assistant})
+            for assistant in self.settings.get('assistants', [])
         ]
 
     def on_input(self, region: Optional[Region], text: Optional[str], view: View, mode: str, assistant: AssistantSettings, input: str):
@@ -75,3 +81,8 @@ class OpenaiPanelCommand(WindowCommand):
         if cls.worker_thread and cls.worker_thread.is_alive():
             cls.stop_event.set()  # Signal the thread to stop
             cls.worker_thread = None
+
+    def __del__(self):
+        # Make sure to remove the settings change callback when the object is deleted
+        if self.settings:
+            self.settings.clear_on_change("reload_assistants")
